15cf44cf06167062671d44afc3da23b7e88b0108
Fernando Dantas <fernandodantas1996@gmail.com>
2020-02-29 11:03:02 -0300

    gif example

--- chaos.py
+++ chaos.py
@@ -1,6 +1,6 @@
 from collider import Disk, System
 
 if __name__ == '__main__':
-    disks = [Disk(radius=0.01) for _ in range(100)]
-    system = System(disks, width=700, height=700)
+    disks = [Disk(radius=0.025) for _ in range(50)]
+    system = System(disks)
     system.simulate(1000, 24)
