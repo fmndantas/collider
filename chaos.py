7e6ce480f511ce5d799c72f172e42e58ba50f43b
Fernando Dantas <fernandodantas1996@gmail.com>
2020-02-29 11:08:46 -0300

    updated gif

--- chaos.py
+++ chaos.py
@@ -3,4 +3,4 @@ from collider import Disk, System
 if __name__ == '__main__':
     disks = [Disk(radius=0.025) for _ in range(50)]
     system = System(disks)
-    system.simulate(1000, 24)
+    system.simulate(1000, 100)
